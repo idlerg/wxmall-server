f95f3b5ee0909f046d425353c4d50a2533518909
betrayalFan <3219699854@qq.com>
2021-06-08 12:57:04 +0800

    增删改查

--- routes/test.js
+++ routes/test.js
@@ -1,34 +1,68 @@
 const express = require('express')
 var router = express.Router()
-
 const test = require('../models/test')
 
-// 获取数据
+// 获取全部数据 √
 router.get('/getData', function(req, res, next) {
     test.find({},function(err, docs) {
         if(err) {
             res.json({
-                code: 100001,
+                code: 10001,
                 msg: err.message
             })
         } else {
             res.json({
                 code: 0,
-                msg: '',
                 data: docs
             })
         }
     })
 })
 
-// 提交数据
+// 按条件查询数据 √
+router.post('/getCondData', function(req, res, next) {
+    const params = req.body
+    test.find(params,function(err, docs) {
+        if(err) {
+            res.json({
+                code: 10001,
+                msg: err.message
+            })
+        } else {
+            res.json({
+                code: 0,
+                data: docs
+            })
+        }
+    })
+})
+
+// 提交数据 √
 router.post('/setData', function(req, res, next){
     const params = req.body
     test.insertMany(params,function(err, docs) {
         if(err) {
             res.json({
                 code: 10001,
-                msg: msg.message
+                msg: err.message
+            })
+        } else {
+            res.json({
+                code: 0,
+                data: true
+            })
+        }
+    })
+})
+
+// 修改数据
+router.post('/modifData', function (req, res, next){
+    const params = req.body
+    test.updateOne({name: params.name}, {name: params.newName}, function (err, docs) {
+        if(err) {
+            res.json({
+                code: 10001,
+                data: err.message
             })
         } else {
             res.json({
@@ -39,4 +73,23 @@ router.post('/setData', function(req, res, next){
     })
 })
 
+// 删除数据
+router.post('/deleteData', function (req, res, next){
+    const params = req.body
+    test.deleteOne(params, function (err, docs) {
+        if(err) {
+            res.json({
+                code: 10001,
+                data: err.message
+            })
+        } else {
+            res.json({
+                code: 0,
+                data: {}
+            })
+        }
+    })
+})
+
+ 
 module.exports = router
